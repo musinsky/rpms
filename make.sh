710ff32fad2fa7c9a92db3af6669d69672226298
Jan Musinsky <musinsky@gmail.com>
2023-04-04 00:54:54 +0200

    using local spec file

--- make.sh
+++ make.sh
@@ -1,13 +1,15 @@
-# 2023-04-03
+# 2023-04-04
 
 # TODO rewrite as Makefile
 
 PACKAGE='hunspell-sk'
 
-GHRAW="https://raw.githubusercontent.com/musinsky/rpms/rawhide/$PACKAGE"
+# GHRAW="https://raw.githubusercontent.com/musinsky/rpms/rawhide/$PACKAGE"
+# RPM_SOURCE_DIR=$(rpm --eval %{_sourcedir})
 RPM_SPEC_DIR=$(rpm --eval %{_specdir})
-RPM_SOURCE_DIR=$(rpm --eval %{_sourcedir})
 
-wget --timestamping "$GHRAW/$PACKAGE.spec" --directory-prefix="$RPM_SPEC_DIR"
+rpmdev-setuptree
+# wget --timestamping "$GHRAW/$PACKAGE.spec" --directory-prefix="$RPM_SPEC_DIR"
+cp -p "$PACKAGE/$PACKAGE.spec" "$RPM_SPEC_DIR/$PACKAGE.spec"
 rpmdev-spectool --get-files --sourcedir "$RPM_SPEC_DIR/$PACKAGE.spec"
 rpmbuild -bs "$RPM_SPEC_DIR/$PACKAGE.spec"   # build just the source package
