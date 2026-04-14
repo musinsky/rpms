0b5165b704e7e52772515b3bcaa6ae002292469a
Jan Musinsky <musinsky@gmail.com>
2026-04-14 23:32:57 +0200

    [root5] specific downloaded dir

--- root5/root5.get.root.master.files.sh
+++ root5/root5.get.root.master.files.sh
@@ -3,7 +3,8 @@
 # 2026-04-14
 # https://github.com/musinsky/rpms/blob/rawhide/root5/root5.get.root.master.files.sh
 
-CUR_DIR="$PWD"
+FINAL_DIR="$PWD"
+[[ -d "$1" ]] && FINAL_DIR="$(realpath "$1")"
 ROOT_GH="https://raw.githubusercontent.com/root-project/root/master"
 ROOT_API="https://api.github.com/repos/root-project/root/commits"
 
@@ -43,5 +44,5 @@ LAST_DATE="$RMKDEPEND_DATE"
 LAST_DATE=$(date --date "$LAST_DATE" +%F)
 TGZ_FILE="root5.with.root.master.$LAST_DATE.files.tar.gz"
 tar -czf "$TGZ_FILE" "$RMKDEPEND_DIR5" "$MINICERN_DIR5"
-mv --verbose "$TGZ_FILE" "$CUR_DIR/"
+mv --verbose "$TGZ_FILE" "$FINAL_DIR/"
 rm -rf "$ROOT5_DIR"
