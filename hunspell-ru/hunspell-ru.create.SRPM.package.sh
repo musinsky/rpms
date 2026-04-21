8241e07cc365ead8264e6ad8e153895514487b05
Jan Musinsky <musinsky@gmail.com>
2026-04-21 23:24:20 +0200

    [hunspell-ru] add hunspell-ru/hunspell-ru.create.SRPM.package.sh

---
+++ hunspell-ru/hunspell-ru.create.SRPM.package.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# 2026-04-21
+# https://github.com/musinsky/rpms/blob/rawhide/hunspell-ru/hunspell-ru.create.SRPM.package.sh
+
+check_command() {
+    local cmd="$1"
+    local rpm="$2"
+    command -v "$cmd" > /dev/null || {
+        printf "# missing command '%s' => " "$cmd"
+        printf "$ sudo dnf install %s\n" "$rpm"
+        exit
+    }
+}
+
+check_command rpmbuild rpm-build
+check_command rpmdev-spectool rpmdevtools
+
+GHC="https://raw.githubusercontent.com/musinsky/rpms/rawhide/hunspell-ru/"
+FSPEC="hunspell-ru.spec"
+SPECS_DIR="$(rpm --eval '%{_specdir}')"
+
+# download specfile
+curl --silent --location "$GHC/$FSPEC" \
+     --remote-name --output-dir "$SPECS_DIR" --create-dirs
+
+# download sources and patches from specfile
+rpmdev-spectool --get-files --sourcedir "$SPECS_DIR/$FSPEC"
+
+# build SRPM package
+rpmbuild -bs "$SPECS_DIR/$FSPEC"   # -bs (source), -bb (binary), -ba (all)
